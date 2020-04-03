40eeac23a1f402854262d29b62147bb40b9c547d
Austin <austintw1018@yahoo.com.tw>
2020-04-04 07:40:49 +0800

    Day 1-normal distribution

--- simulator.c
+++ simulator.c
@@ -16,7 +16,8 @@ int S(int p, int m, char d){
     int request[p];      // processor's request
     int access[p];       // processor access counter
     int memory[m];       // 0 for available, 1 for taken
-    float w=0;
+    float p_average[p]; //time-cumulative average of the access-time for each processor
+    float w=0;  //time-cumulative average of the access-time for all processors
 
     reset(access, p);
 
@@ -45,10 +46,10 @@ int S(int p, int m, char d){
     }
     else{   //normal distribution
         float new_w;    //the new average for the new cycle
-        for(int i=0; i<1000000; ++i){   //limited to a max of 10^6 cycles
+        for(int c=0; c<1000000; ++c){   //limited to a max of 10^6 cycles
 
 
-            
+
 
             if(abs(1-w/new_w)<0.02){    //The system average access time between the current W(new_w) and previous average W (w) is less than a certain tolerance ε(2%)
                 w=new_w;
