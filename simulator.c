a509bc8b183dbf15e2817822905f866d885b7de7
Joshua <joshuajsun@MSI.localdomain>
2020-04-05 12:53:09 -0700

    finished simulation for normal distribution

--- simulator.c
+++ simulator.c
@@ -23,11 +23,15 @@ double generateGaussian() {
 	return g;
 }
 
-// Parameters: p processors, m memory modules, and d for distribution
-// Return: arithmetic average W¯ (Sc(p, m, d)) of all processors time-cumulative averages
+/** 
+* Parameters: p processors, m memory modules, and d for distribution
+* Return: arithmetic average W¯ (Sc(p, m, d)) of all processors time-cumulative averages
+*/
 float S(int p, int m, char d){
 
-    int request[p];      // processor's request 
+    int request[p];        // processor's request 
+    float w = 0;           // time-cumulative average of the access-time for all processors
+
     //reset request for all processors
     for(int i=0; i<p; i++) {
         request[i] = -1;
@@ -35,64 +39,94 @@ float S(int p, int m, char d){
     int access[p];       // keeps track of number of granted accesses for each processor
     int memory[m];       // 0 for available, 1 for taken
     float p_average[p];  // time-cumulative average of the access-time for each processor
-    //reset the access-time for all processors
+
+    // initial access time is 0
     for(int i=0; i<p; i++) {
         p_average[i] = 0;
     }
-    float w=0;           // time-cumulative average of the access-time for all processors
 
-    reset(access, p);   // reset the access counter
-    reset(memory, p);   // reset all memory to available
+    reset(access, p);     // reset the access counter
+    reset(memory, m);     // reset all memory to available
 
-    if(d == 'n'){  // normal distribution
-        // each processor selects random memory module that will serve as mean
+    if(d == 'n') {  // normal distribution
+        
+        // choose random memory module as mean for normal distribution
         int u_p[p];
         for(int i=0; i<p; i++) {
             u_p[i] = rand()%p;
         }
         
-        int start = 0;
-        for(int c=0; c<1000000; c++) {  // limited to a max of 10^6 cycles
+        int start = 0;  // starting from 0th processor
 
-            reset(memory, p); // free memory before each cycle
+        for(int c=1; c<=1000000; c++) {  // limited to a max of 10^6 cycles
 
-            int sum = 0;      // sum of all time-cumulative averages
+            float new_w = 0;
+            reset(memory, m); // free memory before each cycle
 
-            for(int j=start; j<p+start; j++) { // first processor in array gets priority
-                int p = j%p;    
-                
-                int req;
-                if(request[p]) {    // if previous request for memory failed
-                    req = request[p];
-                } else {            // generate new request
-                    int x = round(generateGaussian()*(m/6) + u_p[j]);  // generate random number in normal distribution
+            // -1 means request did not fail
+            for(int i=0; i<p; i++) {
+              request[i] = -1;
+            }
+
+            int accessFail = 0; // boolean to keep track of whether we found a starving processor yet
+
+            // computing new p_average for each processor starting from the previous starving processor
+            for(int j=start; j<p+start; j++) {
+
+                int p_cur = j%p;    // the current processor
+                int req;            // store next request
+
+                // if previous request for memory failed
+                if(request[p_cur] != -1) {    
+                    req = request[p_cur];
+                } 
+                // generate new request
+                else {   
+                    int x = round(generateGaussian()*(m/6) + u_p[p_cur]);  // generate random number in normal distribution
                     req = x % m;
+                    if(req<0) {
+                        req *= -1;
+                    }
                 }
-            
-                
+
                 // if requested memory is free
-                if(req<m && !memory[req]) {   
-                    memory[req] = 1;          // mark memory as taken
-                    access[p]++;                  // increment num of accesses for processor p 
-                    p_average[p] = c/access[p];   // update time-cumulative average for processor p
-                    request[p] = 0;               // make sure we do not store previous request
+                if(!memory[req]) {   
+                    memory[req] = 1;                                    // mark memory as taken
+                    access[p_cur]++;                                    // increment num of accesses for processor p_cur 
+                    p_average[p_cur] = (float)c/(float)access[p_cur];   // update time-cumulative average for processor p_cur
+                    request[p_cur] = -1;                                // make sure request is marked as 'not failed==-1'
                 }
                 // memory is not free
                 else {
-                    p_average[p] = (float)c/access[p];   // update time-cumulative average for processor p
-                    request[p] = req;                // remember previous request
+                    if(access[p_cur]) p_average[p_cur] = (float)c/(float)access[p_cur];   // update time-cumulative average for processor p
+                    request[p_cur] = req;                                                 // remember previous request (request[p_cur] != -1 means it failed)
+                    // if starving processor is not yet chosen
+                    if(!accessFail) { 
+                        start = p_cur;  // update next starting processor                           
+                        accessFail = 1; // mark as chosen
+                    }                    
                 }
-                sum += p_average[p];
             }
 
-            float new_w = sum/p; 
-            if(new_w!=0 && fabs(1-w/new_w)<0.02){   
+            // compute w (average p_average for all processors)
+            for(int i=0; i<p; i++) {
+                // if processor never accessed, mark with infinite time and quit computing
+                if(p_average[i] == 0){   
+                    new_w = FLT_MAX;
+                    break;
+                }
+                new_w += p_average[i];      // summing p_averages
+            }
+            if(new_w != FLT_MAX) new_w /= p; // compute average
+            
+            // 2% tolerance and assuming all processors have been accessed at least once
+            if(new_w!=0 && new_w!=FLT_MAX && fabs(1-w/new_w)<0.02) { 
                 w = new_w;
                 break;
-            }
-            w = new_w;
+            } else {
+                w = new_w;
+            }    
         }
-
 
     } else{   //uniform distribution
         int start=0;    //the first starving processor
@@ -201,7 +235,7 @@ int main(int argc, char** argv) {
     int p = atoi(argv[1]);
     char d = *argv[2];
     
-    //The simulation part
+    // The simulation part
     for(int i=0; i<2048; ++i){
         printf("Processors:%d Memory modules:%d Distribution:%c\t", p, i+1, d);
         printf("W:%.4f\n", S(p,i+1,d));
